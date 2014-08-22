cfca92fe2027167a756c7e13e7d330444c49f2aa
Joshua Howland <development@jkhowland.com>
2014-08-22 14:08:49 -0600

    Adds objective c base project

---
+++ FizzBuzz-Objc/FizzBuzz-Objc/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  FizzBuzz-Objc
+//
+//  Created by Joshua Howland on 8/22/14.
+//  Copyright (c) 2014 DevMountain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ FizzBuzz-Objc/FizzBuzz-Objc/AppDelegate.m
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.m
+//  FizzBuzz-Objc
+//
+//  Created by Joshua Howland on 8/22/14.
+//  Copyright (c) 2014 DevMountain. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    
+    // Insert code here
+    
+    return YES;
+}
+
+@end
